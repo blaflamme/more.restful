65f8d738fc2d502c9a8b13b910cbb9bba49722c4
Blaise Laflamme <blaise@laflamme.org>
2017-01-22 14:45:26 -0500

    add perms by action type: view, edit, add, delete

--- more/restful/abc.py
+++ more/restful/abc.py
@@ -6,3 +6,44 @@ class Resource(abc.ABC):
     def __init__(self, obj, request):
         self.obj = obj
         self.request = request
+
+
+class ViewableResource(abc.ABC):
+
+    @abc.abstractmethod
+    def asdict(self):
+        raise NotImplemented()
+
+
+class EditableResource(abc.ABC):
+
+    @abc.abstractmethod
+    def validate(self, data, partial):
+        raise NotImplemented()
+
+    @abc.abstractmethod
+    def update_data(self, data, replace):
+        raise NotImplemented()
+
+    def complete_data(self, data):
+        # TODO merge data here
+        return self.asdict()
+
+    @abc.abstractmethod
+    def asdict(self):
+        raise NotImplemented()
+
+
+class DeletableResource(abc.ABC):
+
+    @abc.abstractmethod
+    def delete(self):
+        raise NotImplemented()
+
+
+class CollectionResource(abc.ABC):
+    def validate_resource(self, data):
+        raise NotImplemented()
+
+    def add_resource(self, data):
+        raise NotImplemented()
