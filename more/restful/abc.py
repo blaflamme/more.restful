1cc400ff64aa7a2d7fec6e9a457a9f7553ce46dd
Blaise Laflamme <blaise@laflamme.org>
2017-01-22 19:36:24 -0500

    no cover abc classes

--- more/restful/abc.py
+++ more/restful/abc.py
@@ -14,37 +14,37 @@ class ViewableResource(abc.ABC):
 
     @abc.abstractmethod
     def asdict(self):
-        raise NotImplemented()
+        raise NotImplemented()  # pragma: no cover
 
 
 class EditableResource(abc.ABC):
 
     @abc.abstractmethod
     def validate(self, data, partial):
-        raise NotImplemented()
+        raise NotImplemented()  # pragma: no cover
 
     @abc.abstractmethod
     def update_data(self, data, replace):
-        raise NotImplemented()
+        raise NotImplemented()  # pragma: no cover
 
     def complete_data(self, data):
         return remerge([self.asdict(), data])
 
     @abc.abstractmethod
     def asdict(self):
-        raise NotImplemented()
+        raise NotImplemented()  # pragma: no cover
 
 
 class DeletableResource(abc.ABC):
 
     @abc.abstractmethod
     def delete(self):
-        raise NotImplemented()
+        raise NotImplemented()  # pragma: no cover
 
 
 class CollectionResource(abc.ABC):
     def validate_resource(self, data):
-        raise NotImplemented()
+        raise NotImplemented()  # pragma: no cover
 
     def add_resource(self, data):
-        raise NotImplemented()
+        raise NotImplemented()  # pragma: no cover
